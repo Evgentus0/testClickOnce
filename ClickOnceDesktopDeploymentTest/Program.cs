257e8ea7bae3ff37954ab3f03916acc787ff5ea7
Eugene <zerom2016romanenko@gmail.com>
2021-07-22 20:05:53 +0300

    init commit

---
+++ ClickOnceDesktopDeploymentTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClickOnceDesktopDeploymentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v = 1.2;
+
+            Console.WriteLine($"Hello World!. V{v}");
+
+            Console.ReadKey();
+        }
+    }
+}
